749b47a6c2e7f2788e868979b24aa880661d5f23
8409251542 <ravi.yoof@gmail.com>
2026-08-12 17:04:21 +0530

    Move Firebase credentials to environment variables (.env)

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const crypto = require('crypto');
 require('dotenv').config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0mvXTWDmDV_PkgGAEMJn0zl0HxxhzDWo",
-  authDomain: "socialrecharge.firebaseapp.com",
-  databaseURL: "https://socialrecharge.firebaseio.com",
-  projectId: "project-1956585320571671692",
-  storageBucket: "project-1956585320571671692.appspot.com",
-  messagingSenderId: "194244686544",
-  appId: "1:194244686544:web:8f351790aa7a5bcc71e4d5",
-  measurementId: "G-J4DY04HZXP"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = express();
